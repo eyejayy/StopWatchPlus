f3ae2dc1e18310707206cfc4be8f255187a7a612
justinbullock <jgb22@njit.edu>
2017-07-29 16:53:11 -0400

    Added button and textview

--- app/src/main/java/com/bignerdranch/android/stopwatchplus/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/stopwatchplus/MainActivity.java
@@ -2,12 +2,26 @@ package com.bignerdranch.android.stopwatchplus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button submit_button;
+    private TextView answer_box;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set components - start
+        //probability_input = (EditText) findViewById(R.id.edit_probability);
+        //projected_return_input = (EditText) findViewById(R.id.edit_return);
+        //calculate_button = (Button) findViewById(R.id.calculate_button);
+        submit_button = (Button) findViewById(R.id.submit_button);
+        answer_box = (TextView) findViewById(R.id.answer_box);
+        //set components - end
     }
 }
